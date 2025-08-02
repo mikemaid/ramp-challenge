3280b96e8b36438add199d66a59652424aa5bfe3
Aaron Qi <37227990+mikemaid@users.noreply.github.com>
2025-08-02 03:27:31 +0000

    Fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  allowedHosts: true,
+  server: {
+    plugins: [react()],
+    allowedHosts: true,
+  },
 });
