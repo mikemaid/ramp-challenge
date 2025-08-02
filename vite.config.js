c489b00b7bb3cd0dbcf561d3549ac90c40147770
Aaron Qi <37227990+mikemaid@users.noreply.github.com>
2025-08-02 03:32:48 +0000

    FIX

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   server: {
-    plugins: [react()],
-    allowedHosts: true,
+    allowedHosts: [".csb.app"],
   },
 });
